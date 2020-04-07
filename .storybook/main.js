2e06b6caa4b0859e0df020524575bb1d33058fd8
Steve Zhu <st.zhu1@gmail.com>
2020-04-07 18:46:21 -0400

    Setup project (#1)

---
+++ .storybook/main.js
@@ -0,0 +1,12 @@
+module.exports = {
+  stories: ['../src/**/*.stories.[tj]sx'],
+  addons: ['@storybook/addon-docs', '@storybook/addon-storysource'],
+  webpackFinal: (config) => {
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      use: ['ts-loader', 'react-docgen-typescript-loader'],
+    })
+    config.resolve.extensions.push('.ts', '.tsx')
+    return config
+  },
+}
---
+++ src/SortableList/SortableList.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { SortableList } from '.'
+
+export default {
+  title: 'SortableList',
+  components: SortableList,
+}
+
+export const withDefault = () => {
+  return <SortableList />
+}
